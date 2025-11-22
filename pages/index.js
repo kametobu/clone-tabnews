c32e4bc610e1a1d2061b8df8ed2effbb91f61538
Alann Batista Cardoso Silva <65514891+kametobu@users.noreply.github.com>
2025-11-22 16:00:13 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Ninguém vai bater tão forte quanto a vida. A questão não é o quão forte você bate, mas o quanto você aguenta apanhar e continuar seguindo em frente</h1>
+    return <h1>Ninguém vai bater tão forte quanto a vida. A questão não é o quão forte você bate, mas o quanto você aguenta apanhar e continuar seguindo em frente.</h1>
 }
 
 export default Home;
