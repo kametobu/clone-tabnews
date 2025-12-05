141bc018a7537abb568b9009c54e18e23eec4ff5
Alann Batista Cardoso Silva <65514891+kametobu@users.noreply.github.com>
2025-12-05 14:15:17 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,8 @@
-function status(request, response) {
+import databasse from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await databasse.query("SELECT 1 + 1 AS result;");
+  console.log(result.rows[0]);
   response.status(200).json({ status: "ok" });
 }
 
